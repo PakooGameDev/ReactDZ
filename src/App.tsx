bcc7c35eeaf99b32ba6c63302bd692e10bee1c79
PakooGD <popkov-lelya89@mail.ru>
2023-09-28 13:50:21 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import ColorChangeButton from "./ColorChangeButton";
+import { mockTasks } from "./mockData";
+import TaskList from "./TaskList";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ColorChangeButton />
+      <h1>Список задач</h1>
+      <TaskList tasks={mockTasks} />
     </div>
   );
-}
+};
 
 export default App;
